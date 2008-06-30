8d5f84e1cdac435a3cba9bf18c614936d29b7600
Eitan Isaacson <eitan@ascender.com>
2008-06-30 10:36:34 -0700

    Added copyright in headers.

--- pyia/accessible.py
+++ pyia/accessible.py
@@ -1,3 +1,30 @@
+'''
+Creates functions at import time that are mixed into the 
+IAccessible base class to make it more Pythonic.
+
+Inspired by pyatspi:
+http://live.gnome.org/GAP/PythonATSPI
+
+@author: Eitan Isaacson
+@copyright: Copyright (c) 2008, Eitan Isaacson
+@license: LGPL
+
+This library is free software; you can redistribute it and/or
+modify it under the terms of the GNU Library General Public
+License as published by the Free Software Foundation; either
+version 2 of the License, or (at your option) any later version.
+
+This library is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+Library General Public License for more details.
+
+You should have received a copy of the GNU Library General Public
+License along with this library; if not, write to the
+Free Software Foundation, Inc., 59 Temple Place - Suite 330,
+Boston, MA 02111-1307, USA.
+'''
+
 import new
 import types
 from comtypes.automation import VARIANT, VT_I4, VT_DISPATCH
--- pyia/registry.py
+++ pyia/registry.py
@@ -1,3 +1,30 @@
+'''
+A registry, sort of like AT-SPI has. For now it's just an entry point for 
+registering liteners for MSAA events.
+
+Inspired by pyatspi:
+http://live.gnome.org/GAP/PythonATSPI
+
+@author: Eitan Isaacson
+@copyright: Copyright (c) 2008, Eitan Isaacson
+@license: LGPL
+
+This library is free software; you can redistribute it and/or
+modify it under the terms of the GNU Library General Public
+License as published by the Free Software Foundation; either
+version 2 of the License, or (at your option) any later version.
+
+This library is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+Library General Public License for more details.
+
+You should have received a copy of the GNU Library General Public
+License along with this library; if not, write to the
+Free Software Foundation, Inc., 59 Temple Place - Suite 330,
+Boston, MA 02111-1307, USA.
+'''
+
 import constants
 from ctypes import CFUNCTYPE, c_int, c_voidp, windll
 from comtypes.client import PumpEvents
